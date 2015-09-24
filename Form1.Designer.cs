dcd65375972523a139bd275853caf7eb07b9751e
reymnia <reyhanehmnia@gmail.com>
2015-09-24 16:47:32 -0400

    GUI
    GUI

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ok_button = new System.Windows.Forms.Button();
+            this.cancel_button = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.browse_button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.practiceID_textBox = new System.Windows.Forms.TextBox();
+            this.fileDir_textBox = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ok_button
+            // 
+            this.ok_button.Location = new System.Drawing.Point(90, 385);
+            this.ok_button.Name = "ok_button";
+            this.ok_button.Size = new System.Drawing.Size(183, 44);
+            this.ok_button.TabIndex = 1;
+            this.ok_button.Text = "OK";
+            this.ok_button.UseVisualStyleBackColor = true;
+            this.ok_button.Click += new System.EventHandler(this.ok_button_Click);
+            // 
+            // cancel_button
+            // 
+            this.cancel_button.Location = new System.Drawing.Point(279, 385);
+            this.cancel_button.Name = "cancel_button";
+            this.cancel_button.Size = new System.Drawing.Size(183, 44);
+            this.cancel_button.TabIndex = 2;
+            this.cancel_button.Text = "Cancel";
+            this.cancel_button.UseVisualStyleBackColor = true;
+            this.cancel_button.Click += new System.EventHandler(this.cancel_button_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // browse_button
+            // 
+            this.browse_button.Location = new System.Drawing.Point(481, 152);
+            this.browse_button.Name = "browse_button";
+            this.browse_button.Size = new System.Drawing.Size(190, 40);
+            this.browse_button.TabIndex = 5;
+            this.browse_button.Text = "Browse My Computer";
+            this.browse_button.Click += new System.EventHandler(this.browse_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Practice ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "File Directory";
+            // 
+            // practiceID_textBox
+            // 
+            this.practiceID_textBox.Location = new System.Drawing.Point(210, 56);
+            this.practiceID_textBox.Name = "practiceID_textBox";
+            this.practiceID_textBox.Size = new System.Drawing.Size(254, 26);
+            this.practiceID_textBox.TabIndex = 3;
+            // 
+            // fileDir_textBox
+            // 
+            this.fileDir_textBox.Location = new System.Drawing.Point(210, 159);
+            this.fileDir_textBox.Name = "fileDir_textBox";
+            this.fileDir_textBox.Size = new System.Drawing.Size(254, 26);
+            this.fileDir_textBox.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.fileDir_textBox);
+            this.panel1.Controls.Add(this.practiceID_textBox);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.browse_button);
+            this.panel1.Location = new System.Drawing.Point(69, 74);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(705, 253);
+            this.panel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(839, 508);
+            this.Controls.Add(this.cancel_button);
+            this.Controls.Add(this.ok_button);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button ok_button;
+        private System.Windows.Forms.Button cancel_button;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button browse_button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox practiceID_textBox;
+        private System.Windows.Forms.TextBox fileDir_textBox;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // This button browses for the file, and writes the file's path in the File Directory textbox.
+        private void browse_Click(object sender, System.EventArgs e)
+        {
+            
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog1.FileName;
+                fileDir_textBox.Text = filePath;
+
+            }
+        }
+
+        // This button checks for the textboxes to be filled out. In case they're not, would prompt the user for an input.
+        // Else, would save the textbox values in the params.txt file in MyDocuments folder and closes the window.
+        private void ok_button_Click(object sender, EventArgs e)
+        {
+            if (practiceID_textBox.Text != "")
+            {
+                if (fileDir_textBox.Text != "")
+                {
+
+                    // Set a variable to the My Documents path.
+                    string mydocpath =
+                        Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                    // Write the params to a new file named "params.txt".
+                    using (StreamWriter outputFile = new StreamWriter(mydocpath + @"\param.txt"))
+                    {
+                            outputFile.WriteLine(practiceID_textBox.Text);
+                            outputFile.WriteLine(fileDir_textBox.Text);
+
+                            MessageBox.Show("A text file with the parameters has been created in MyDocuments folder.");
+                            this.Close();
+                    }
+
+
+                }
+                else {
+                    MessageBox.Show ("Please enter the File Directory.");
+                    return;
+                }
+            }
+            else {
+                MessageBox.Show("Please enter the Practice ID.");
+                return;
+            }
+        }
+
+        // This button closes the window
+        private void cancel_button_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
